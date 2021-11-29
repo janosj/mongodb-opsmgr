553f76ae2efeb846dc560216d5e17bd3b068dae6
jonathan.janos@mongodb.com <jonathan.janos@mongodb.com>
2021-11-29 18:59:35 -0500

    fix

--- opsmgr-vm/upgrade.5.0.sh
+++ opsmgr-vm/upgrade.5.0.sh
@@ -14,14 +14,21 @@ echo "Stopping any running Docker containers..."
 echo "Shutting down Ops Manager..."
 service mongodb-mms stop
 
+# Cleanup the image
+
 echo "Reclaiming space..."
 ./reclaim-space.sh
 
-echo "Remember to clear up /etc/hosts..."
-# To Do: clear up /etc/hosts
+echo "Clearing up /etc/hosts..."
+sed -i '/myserver/d' /etc/hosts
+
+# End image cleanup
 
 echo "Preserving old config files..."
+# conf-mms.properties uses two additional lines to support local mode (added subsequently)
 cp /opt/mongodb/mms/conf/conf-mms.properties /opt/mongodb/mms/conf/conf-mms.properties.4.4.12
+# This changes from version to version, but it's not customized here
+cp /opt/mongodb/mms/conf/mms.conf /opt/mongodb/mms/conf/mms.conf.4.4.12
 cp /etc/mongodb-mms/gen.key /etc/mongodb-mms/gen.key.4.4.12
 
 echo "Downloading Ops Manager binary..."
@@ -36,10 +43,16 @@ else
     echo "  .. already downloaded."
 fi
 
-echo "Installing new OM package..."
+echo "Installing new OM package... Selet Y to overwrite existing files."
 dpkg -i $downloadFolder/$omFile
 
 # Update get_versions.sh to include 100.3.1 or later of Tools (as required by OM 5)
+
+echo "Restoring local mode modifications in conf-mms.properties..."
+cat <<EOT >> /opt/mongodb/mms/conf/conf-mms.properties
+automation.versions.source=local
+automation.versions.directory=/opt/mongodb/mms/mongodb-releases/
+EOT
 
 echo "Starting Ops Manager..."
 sudo service mongodb-mms start
