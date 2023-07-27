be424a77fbe4bf8d0681034632bd0054d11bf1dc
Jonathan Janos <janosj@yahoo.com>
2023-07-27 16:11:09 -0400

    edits

--- opsmgr-aws/1-install-opsMgr/2-get-server-cert.sh
+++ opsmgr-aws/1-install-opsMgr/2-get-server-cert.sh
@@ -1,17 +1,23 @@
 echo "Run this from your laptop to pull down the Ops Manager CA file."
 echo "Your web browser needs this to access the Ops Manager UI."
-echo "The agents also need it to connect."
+echo "The agents also need it to connect to Ops Manager."
 echo
 
 echo "Your AWS key is required to access the Ops Manager instance."
-read -p "Name of your AWS keyfile (no extension): " KEYFILE
+echo "This script is wired to look for it in the $HOME/Keys/ directory."
+read -p "Enter the name of your AWS keyfile (no extension): " KEYFILE
 
 # opsmgr-aws has to be an entry in your local hosts file.
-echo "Pulling cert from opsmgr-aws..."
+echo "Downloading Ops-Manager cert from opsmgr-aws to local Downloads folder..."
 scp -i $HOME/Keys/$KEYFILE.pem ec2-user@opsmgr-aws:./opsmgrCA.pem ~/Downloads/
 
 echo "Server cert successfully downloaded (~/Downloads/opsmgrCA.pem)."
-echo "Import it via Keychain Access (Mac utility, drag and drop into login section)"
-echo "Double-click on it, switch Trust to 'Always Trust'"
+echo "Import it via Keychain Access. Launch the Mac utility, "
+echo "select the Default login Keychain on the left, "
+echo "then the Certificates tab on the top-right, "
+echo "and drag and drop the downloaded opsmgrCA.pem file into this space."
+echo "Then, double-click on it and switch Trust to 'Always Trust'"
+echo "If this is done correctly, you can access the Ops Manager UI"
+echo "without out seeing any privacy warnings." 
 
 
--- opsmgr-aws/1-install-opsMgr/README.md
+++ opsmgr-aws/1-install-opsMgr/README.md
@@ -12,7 +12,7 @@ When the script completes, Ops Manager should be up and running. Note the follow
 
 - The installation script configures Ops Manager for *local mode operation*, meaning the versions of MongoDB version that you want to deploy must be downloaded locally in advance (which is done for you by the *getVersions* script as part of the installation). 
 
-- Ops Manager is configured for SSL using a self-signed certificate. To allow your local browser to accept this certificate, run *get-server-cert* from your local machine and follow the instructions to import it into your local keychain.
+- Ops Manager is configured for SSL using a self-signed certificate. To allow your local browser to accept this certificate, run *get-server-cert* from your local machine and follow the instructions to import it into your local keychain. Test connectivity by accessing the Ops Manager UI at *https:opsmgr-aws:8443*. With the certificate imported correctly, you should no longer see an ERR_CERT_AUTHORITY_INVALID warning in the "Your connection is not private" page. 
 
 Next, proceed with installing the agents.
 
